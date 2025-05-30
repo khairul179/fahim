bb83b11a6b1818f47a784ecdbf610c4d55ce1c75
Khairul Islam B <khairulislamfahim38@gmail.com>
2025-05-30 13:05:11 +0600

    Ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fahim/', // <- this is required for GitHub Pages
+  base: '/fahim/', // IMPORTANT: must match your repo name
   plugins: [react()],
 });
